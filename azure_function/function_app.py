f43c8e513b36927a89fa3d49e80b778d34bf003a
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2024-07-17 12:54:16 +0000

    add logging

--- azure_function/function_app.py
+++ azure_function/function_app.py
@@ -1,8 +1,21 @@
+import logging.config
 import azure.functions as func
-import logging
+import yaml
+
+with open("log_conf.yaml", "rt") as f:
+    config = yaml.safe_load(f.read())
+
+logging.config.dictConfig(config)
+logger = logging.getLogger("root")
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
+
 @app.route(route="Health")
 def Health(req: func.HttpRequest) -> func.HttpResponse:
+    logger.info("Info Health Check")
+    logger.debug("Debug Health Check")
+    logger.error("Error Health Check")
+    logger.warning("Warning Health Check")
+    logger.critical("Critical Health")
     return func.HttpResponse("Healthy", status_code=200)
