f842f2acdda201c97a3dc3fae157838d92117aaa
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2024-07-18 10:50:40 +0000

    fix

--- azure_function/function_app.py
+++ azure_function/function_app.py
@@ -2,17 +2,16 @@ import logging.config
 import logging.handlers
 import azure.functions as func
 from datetime import datetime, timezone
-from azure_function.azure_storage_table_helper import Record
-from azure_function.azure_storage_table_helper import AzureTableStorageHelper
+from azure_storage_table_helper import Record
+from azure_storage_table_helper import AzureTableStorageHelper
 
 
 logger = logging.getLogger("root")
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
-table_helper = AzureTableStorageHelper(logger)
 
 
 @app.function_name(name="Health")
-@app.route(route="Health")
+@app.route(route="Health", methods=["GET"])
 def Health(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse("Healthy", status_code=200)
 
@@ -28,6 +27,7 @@ async def CreateRecord(req: func.HttpRequest) -> func.HttpResponse:
     record: Record = Record(
         "123", "This is a note", 1, True, datetime.now(timezone.utc)
     )
+    table_helper = AzureTableStorageHelper(logger)
     table_helper.set_record(record)
     return func.HttpResponse("Record Created", status_code=200)
 
@@ -40,6 +40,7 @@ async def GetRecord(req: func.HttpRequest) -> func.HttpResponse:
     logger.error("Error GetRecord")
     logger.warning("Warning GetRecord")
     logger.critical("Critical GetRecord")
+    table_helper = AzureTableStorageHelper(logger)
     record = await table_helper.get_record("123")
     if record is None:
         return func.HttpResponse("Record Not Found", status_code=404)
