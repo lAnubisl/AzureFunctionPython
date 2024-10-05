43c01259f17fa679cf1debb72c0a5e5dc9ac25f6
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2024-10-05 13:14:53 +0000

    playing with loggers

--- azure_function/function_app.py
+++ azure_function/function_app.py
@@ -4,7 +4,7 @@ import azure.functions as func
 import opentelemetry.trace
 from azure.monitor.opentelemetry import configure_azure_monitor
 
-configure_azure_monitor()
+# configure_azure_monitor()
 # tracer: opentelemetry.trace.Tracer = opentelemetry.trace.get_tracer(__name__)
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
