4a71822f80c4b3b2939c2905c0c7f5aef1096264
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2024-12-23 10:28:40 +0000

    change the order of imports to match as it is in documentation

--- azure_function/function_app.py
+++ azure_function/function_app.py
@@ -1,11 +1,11 @@
 import logging
 import requests
 import azure.functions as func
-import opentelemetry.trace
 from azure.monitor.opentelemetry import configure_azure_monitor
+from opentelemetry import trace
 
 configure_azure_monitor()
-tracer: opentelemetry.trace.Tracer = opentelemetry.trace.get_tracer(__name__)
+tracer: trace.Tracer = trace.get_tracer(__name__)
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.timer_trigger(schedule="0 */5 * * * *", arg_name="timer")
