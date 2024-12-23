c0db24a1e672f0c4504cb254eef83965863befbe
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2024-12-23 12:34:41 +0000

    remove context parameter from timer trigger

--- azure_function/function_app.py
+++ azure_function/function_app.py
@@ -1,4 +1,5 @@
 
+from typing import Optional
 import logging
 import requests
 import azure.functions as func
@@ -12,13 +13,9 @@ tracer: trace.Tracer = trace.get_tracer(__name__)
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.timer_trigger(schedule="0 */5 * * * *", arg_name="timer")
-async def timer_trigger(timer: func.TimerRequest, context) -> None:
-    ctx: Context = extract({
-      "traceparent": context.trace_context.Traceparent,
-      "tracestate": context.trace_context.Tracestate,
-    })
+async def timer_trigger(timer: func.TimerRequest) -> None:
     logging.info("Call: TimerTrigger Started")
-    await RunBusinessLogic(ctx)
+    await RunBusinessLogic(None)
     logging.info("Call: TimerTrigger Ended")
 
 @app.function_name(name="GetRecord")
@@ -33,11 +30,11 @@ async def GetRecord(req: func.HttpRequest, context) -> func.HttpResponse:
     logging.info("Call: GetRecord Ended")
     return func.HttpResponse("Done", status_code=200)
 
-async def RunBusinessLogic(ctx: Context) -> None:
+async def RunBusinessLogic(ctx: Optional[Context]) -> None:
     with tracer.start_as_current_span("My Business Logic", context=ctx):
-        with tracer.start_as_current_span("HTTP Request to Google", context=ctx):
+        with tracer.start_as_current_span("HTTP Request to Google"):
             google_resp = requests.get(url='https://google.com')
             logging.info(f"google response status code {google_resp.status_code}")
-        with tracer.start_as_current_span("HTTP Request to Microsoft", context=ctx):
+        with tracer.start_as_current_span("HTTP Request to Microsoft"):
             msft_resp = requests.get(url='https://www.microsoft.com')
             logging.info(f"msft response status code {msft_resp.status_code}")
