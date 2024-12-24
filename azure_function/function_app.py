9228a1de12403e6feef00d2f7a3e1cea5ed67067
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2024-12-24 11:03:53 +0000

    rename function arguments

--- azure_function/function_app.py
+++ azure_function/function_app.py
@@ -9,14 +9,14 @@ depencencies_builder = DependenciesBuilder()
 tracer: trace.Tracer = trace.get_tracer(__name__)
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.timer_trigger(schedule="0 */5 * * * *", arg_name="_")
-async def timer_trigger(_: func.TimerRequest, context: func.Context) -> None:
+@app.timer_trigger(schedule="0 */5 * * * *", arg_name="timer")
+async def timer_trigger(timer: func.TimerRequest, context: func.Context) -> None:
     with tracer.start_as_current_span("Execute command", context=transform_context(context)):
         await depencencies_builder.get_command().execute()
 
 @app.function_name(name="GetRecord")
 @app.route(route="GetRecord", methods=["GET"])
-async def GetRecord(_: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+async def GetRecord(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     with tracer.start_as_current_span("Execute command", context=transform_context(context)):
         await depencencies_builder.get_command().execute()
     return func.HttpResponse("Done", status_code=200)
