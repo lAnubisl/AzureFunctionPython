171b7ca82d3392cb66768f1b7a54cc9d13e5cfa0
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2025-07-18 13:31:18 +0000

    remove some code

--- azure_function/dependencies_builder.py
+++ azure_function/dependencies_builder.py
@@ -1,18 +1,12 @@
 import logging
-import opentelemetry
-import opentelemetry.trace
 from command import Command
 
 class DependenciesBuilder:
     def __init__(self):
-        self.tracer: opentelemetry.trace.Tracer = opentelemetry.trace.get_tracer("MyApp")
         self.logger: logging.Logger = logging.getLogger("MyApp")
 
     def get_command(self):
         return Command(self.logger, self.tracer)
-
-    def get_tracer(self) -> opentelemetry.trace.Tracer:
-        return self.tracer
 
     def get_logger(self) -> logging.Logger:
         return self.logger
