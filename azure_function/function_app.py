003cd492dabc263c2209a931c0ec0d20fc3d0a9f
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2025-07-18 13:50:46 +0000

    remove more code

--- azure_function/function_app.py
+++ azure_function/function_app.py
@@ -1,9 +1,6 @@
 import azure.functions as func
-from azure.monitor.opentelemetry import configure_azure_monitor # type: ignore
-from opentelemetry import trace
 from dependencies_builder import DependenciesBuilder
 
-configure_azure_monitor()
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.timer_trigger(schedule="0 0 0 * * *", arg_name="timer")
