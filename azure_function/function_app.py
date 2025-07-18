fff5517b4a09822ee0143cbf702cde2a8e280930
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2025-07-18 12:59:20 +0000

    fix

--- azure_function/function_app.py
+++ azure_function/function_app.py
@@ -8,12 +8,13 @@ from ambient_context_manager import set_context, unset_context, get_context
 configure_azure_monitor(
     logger_name="MyApp",
 )
-depencencies_builder = DependenciesBuilder()
-tracer: trace.Tracer = depencencies_builder.get_tracer()
+
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.timer_trigger(schedule="0 0 0 * * *", arg_name="timer")
 async def timer_trigger(timer: func.TimerRequest, context: func.Context) -> None:
+    depencencies_builder = DependenciesBuilder()
+    tracer: trace.Tracer = depencencies_builder.get_tracer()
     set_context(transform_context(context))
     try:
         with tracer.start_as_current_span("Execute command", context=get_context()):
@@ -24,6 +25,8 @@ async def timer_trigger(timer: func.TimerRequest, context: func.Context) -> None
 @app.function_name(name="GetRecord")
 @app.route(route="GetRecord", methods=["GET"])
 async def GetRecord(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    depencencies_builder = DependenciesBuilder()
+    tracer: trace.Tracer = depencencies_builder.get_tracer()
     set_context(transform_context(context))
         with tracer.start_as_current_span("Execute command", context=get_context()):
             await depencencies_builder.get_command().execute()
