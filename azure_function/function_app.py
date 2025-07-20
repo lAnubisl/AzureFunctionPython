2b8fc470f722e922fce52ac659d79831d6b93f8f
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2025-07-20 18:04:02 +0000

    remove evetything

--- azure_function/function_app.py
+++ azure_function/function_app.py
@@ -1,9 +1,8 @@
+import logging
 import azure.functions as func
-from dependencies_builder import DependenciesBuilder
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.timer_trigger(schedule="0 0 0 * * *", arg_name="timer")
 async def timer_trigger(timer: func.TimerRequest) -> None:
-    depencencies_builder = DependenciesBuilder()
-    await depencencies_builder.get_command().execute()
+    logging.info("Timer trigger function executed")
