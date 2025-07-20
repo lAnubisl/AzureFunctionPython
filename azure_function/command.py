6b876eb496c497bd03a9b7ee2cc39e83ac44e706
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2025-07-20 20:34:30 +0000

    set span attribyte

--- azure_function/command.py
+++ azure_function/command.py
@@ -10,10 +10,12 @@ class Command:
 
     async def execute(self) -> None:
         async with aiohttp.ClientSession() as session:
-            with self.__tracer.start_as_current_span(name="Get Google Page", context=get_context(), kind=SpanKind.CLIENT):
+            with self.__tracer.start_as_current_span(name="Get Google Page", context=get_context(), kind=SpanKind.CLIENT) as span:
+                span.set_attribute("db.system", "MyAPI")
                 async with session.get('https://www.google.com') as google_resp:
                     self.__logger.info(f"google response status code {google_resp.status}")
 
-            with self.__tracer.start_as_current_span(name="Get Microsoft Page", context=get_context(), kind=SpanKind.CLIENT):
+            with self.__tracer.start_as_current_span(name="Get Microsoft Page", context=get_context(), kind=SpanKind.CLIENT) as span:
+                span.set_attribute("db.system", "MyAPI")
                 async with session.get('https://www.microsoft.com') as msft_resp:
                     self.__logger.info(f"msft response status code {msft_resp.status}")
