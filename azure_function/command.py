ae494bebe18da0151b32040f37d714c2855fad82
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2025-07-20 21:10:50 +0000

    span

--- azure_function/command.py
+++ azure_function/command.py
@@ -1,6 +1,6 @@
 from logging import Logger
 import aiohttp
-from opentelemetry.trace import Tracer, SpanKind
+from opentelemetry.trace import Tracer, SpanKind, use_span
 from ambient_context_manager import get_context
 
 class Command:
@@ -9,16 +9,16 @@ class Command:
         self.__tracer: Tracer = tracer
 
     def span(self, name: str):
-        span = self.__tracer.start_as_current_span(name=name, context=get_context(), kind=SpanKind.CLIENT)
+        span = self.__tracer.start_span(name=name, context=get_context(), kind=SpanKind.CLIENT)
         span.set_attribute("db.system", "MyAPI2")
         return span
 
     async def execute(self) -> None:
         async with aiohttp.ClientSession() as session:
-            with self.span(name="Get Google Page"):
+            with use_span(self.span(name="Get Google Page"), end_on_exit=True):
                 async with session.get('https://www.google.com') as google_resp:
                     self.__logger.info(f"google response status code {google_resp.status}")
 
-            with self.span(name="Get Microsoft Page"):
+            with use_span(self.span(name="Get Microsoft Page"), end_on_exit=True):
                 async with session.get('https://www.microsoft.com') as msft_resp:
                     self.__logger.info(f"msft response status code {msft_resp.status}")
