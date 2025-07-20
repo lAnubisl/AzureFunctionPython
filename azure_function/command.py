cdba561693f889538efdb27fd38ca6d768e20ae6
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2025-07-20 20:55:44 +0000

    optimize span

--- azure_function/command.py
+++ azure_function/command.py
@@ -8,14 +8,17 @@ class Command:
         self.__logger = logger
         self.__tracer: Tracer = tracer
 
+    def span(self, name: str):
+        span = self.__tracer.start_as_current_span(name=name, context=get_context(), kind=SpanKind.CLIENT)
+        span.set_attribute("db.system", "MyAPI2")
+        return span
+
     async def execute(self) -> None:
         async with aiohttp.ClientSession() as session:
-            with self.__tracer.start_as_current_span(name="Get Google Page", context=get_context(), kind=SpanKind.CLIENT) as span:
-                span.set_attribute("db.system", "MyAPI")
+            with self.span(name="Get Google Page"):
                 async with session.get('https://www.google.com') as google_resp:
                     self.__logger.info(f"google response status code {google_resp.status}")
 
-            with self.__tracer.start_as_current_span(name="Get Microsoft Page", context=get_context(), kind=SpanKind.CLIENT) as span:
-                span.set_attribute("db.system", "MyAPI")
+            with self.span(name="Get Microsoft Page"):
                 async with session.get('https://www.microsoft.com') as msft_resp:
                     self.__logger.info(f"msft response status code {msft_resp.status}")
