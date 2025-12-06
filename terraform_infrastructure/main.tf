53679ee471fbcd3bca877ba101b069599d9779d9
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2025-12-06 19:17:21 +0000

    Update devcontainer configuration and Terraform resources for Azure Function deployment

--- terraform_infrastructure/main.tf
+++ terraform_infrastructure/main.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 4.28.0"
+      version = "= 4.55.0"
     }
   }
-  required_version = ">= 1.10.0"
+  required_version = ">= 1.14.1"
 
   backend "azurerm" {
   }
@@ -28,12 +28,18 @@ resource "azurerm_storage_account" "st_func" {
   account_replication_type = "LRS"
 }
 
+resource "azurerm_storage_container" "sc_func" {
+  name                  = "myfunction"
+  storage_account_id    = azurerm_storage_account.st_func.id
+  container_access_type = "private"
+}
+
 resource "azurerm_service_plan" "func_plan" {
   name                = "plan-func-python-deployment-26167"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "Y1"
+  sku_name            = "FC1"
 }
 
 resource "azurerm_linux_function_app" "func" {
@@ -79,6 +85,63 @@ resource "azurerm_linux_function_app" "func" {
     ]
   }
 }
+
+
+resource "azurerm_function_app_flex_consumption" "func" {
+  name                = "func-python-26167"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.func_plan.id
+
+  storage_container_type      = "blobContainer"
+  storage_container_endpoint  = "${azurerm_storage_account.st_func.primary_blob_endpoint}${azurerm_storage_container.st_func.name}"
+  storage_authentication_type = "StorageAccountConnectionString"
+  storage_access_key          = azurerm_storage_account.st_func.primary_access_key
+
+  https_only                                     = true
+  webdeploy_publish_basic_authentication_enabled = false
+
+  runtime_name           = "python"
+  runtime_version        = "3.11"
+  maximum_instance_count = 2
+  instance_memory_in_mb  = 512
+
+  app_settings = {
+    # https://learn.microsoft.com/en-us/azure/azure-functions/functions-app-settings#python_enable_worker_extensions
+    PYTHON_ENABLE_WORKER_EXTENSIONS = "1"
+
+    STORAGE_ACCOUNT_NAME = azurerm_storage_account.st_func.name
+    STORAGE_TABLE_NAME   = azurerm_storage_table.st_tbl_records.name
+
+    # https://learn.microsoft.com/en-us/troubleshoot/azure/azure-monitor/app-insights/telemetry/opentelemetry-troubleshooting-python#duplicate-trace-logs-in-azure-functions
+    # ### Duplicate trace logs in Azure Functions ###
+    # If you see a pair of entries for each trace log within Application Insights, you probably enabled the following types of logging instrumentation:
+    # The native logging instrumentation in Azure Functions
+    # The azure-monitor-opentelemetry logging instrumentation within the distribution
+    # To prevent duplication, you can disable the distribution's logging, but leave the native logging instrumentation in Azure Functions enabled. To do this, set the OTEL_LOGS_EXPORTER environment variable to None.
+    OTEL_LOGS_EXPORTER = "None"
+    # https://learn.microsoft.com/en-us/azure/azure-monitor/app/opentelemetry-configuration?tabs=python#set-the-cloud-role-name-and-the-cloud-role-instance
+    OTEL_SERVICE_NAME        = "MyFunctionApp"
+    OTEL_RESOURCE_ATTRIBUTES = "service.instance.id=MyFunctionApp"
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+  site_config {
+    minimum_tls_version                    = "1.3"
+    application_insights_connection_string = azurerm_application_insights.appi.connection_string
+  }
+  lifecycle {
+    ignore_changes = [
+      app_settings["WEBSITE_ENABLE_SYNC_UPDATE_SITE"],
+      app_settings["WEBSITE_RUN_FROM_PACKAGE"],
+      tags["hidden-link: /app-insights-conn-string"],
+      tags["hidden-link: /app-insights-instrumentation-key"],
+      tags["hidden-link: /app-insights-resource-id"],
+    ]
+  }
+}
+
 
 resource "azurerm_log_analytics_workspace" "logs" {
   name                = "logs-func-python-26167"
