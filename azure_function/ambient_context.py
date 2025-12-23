4329a217da6f0824219abedef1574dfc512f5960
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2025-12-23 09:35:19 +0100

    Fix OpenTelemetry context handling and record formatting

--- azure_function/ambient_context.py
+++ azure_function/ambient_context.py
@@ -1,31 +1,33 @@
 from contextvars import ContextVar
-from opentelemetry.propagate import extract
+
 import azure.functions as func
-import opentelemetry.context as opentelimetry
+from opentelemetry.context import Context
+from opentelemetry.propagate import extract
 
 class AmbientContext:
     """
-    Impelments the Ambient Context pattern
+    Implements the Ambient Context pattern
     """
     def __init__(self):
-        self.__open_telimetry_context = ContextVar(
+        self.__open_telemetry_context = ContextVar(
             "opentelemetry.context",
-            default=opentelimetry.Context())
+            default=Context()
+        )
 
-    def get_opentelimetry_context(self) -> opentelimetry.Context:
+    def get_opentelemetry_context(self) -> Context:
         """
         Get the OpenTelemetry context
         """
-        return self.__open_telimetry_context.get()
+        return self.__open_telemetry_context.get()
 
-    def set_opentelimetry_context(self, context: func.Context) -> None:
+    def set_opentelemetry_context(self, context: func.Context) -> None:
         """
         Set the OpenTelemetry context from the Azure Function context
         Documentation: https://pypi.org/project/azure-monitor-opentelemetry/
         Section: 'Monitoring in Azure Functions'
         """
-        ctx: opentelimetry.Context = extract({
+        ctx: Context = extract({
             "traceparent": context.trace_context.Traceparent, # type: ignore
             "tracestate": context.trace_context.Tracestate, # type: ignore
         }) # type: ignore
-        self.__open_telimetry_context.set(ctx)
+        self.__open_telemetry_context.set(ctx)
