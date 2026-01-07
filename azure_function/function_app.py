f74adb2ab4413dca2d8ac1d3c7c0be174062ef94
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2026-01-07 21:33:04 +0000

    Remove Azure Monitor configuration and enable Application Insights telemetry in app settings

--- azure_function/function_app.py
+++ azure_function/function_app.py
@@ -1,12 +1,8 @@
 import azure.functions as func
 from opentelemetry import trace
 from dependencies_builder import DependenciesBuilder
-from azure.monitor.opentelemetry import configure_azure_monitor # type: ignore
-
-configure_azure_monitor()
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
-
 @app.timer_trigger(schedule="0 0 0 * * *", arg_name="timer")
 async def timer_trigger(timer: func.TimerRequest) -> None:
     builder = DependenciesBuilder()
--- terraform_infrastructure/main.tf
+++ terraform_infrastructure/main.tf
@@ -64,7 +64,7 @@ resource "azurerm_function_app_flex_consumption" "func" {
   app_settings = {
     STORAGE_ACCOUNT_NAME                        = azurerm_storage_account.st_func.name
     STORAGE_TABLE_NAME                          = azurerm_storage_table.st_tbl_records.name
-    # PYTHON_APPLICATIONINSIGHTS_ENABLE_TELEMETRY = true
+    PYTHON_APPLICATIONINSIGHTS_ENABLE_TELEMETRY = true
   }
   identity {
     type = "SystemAssigned"
